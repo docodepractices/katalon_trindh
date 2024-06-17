3f0794e9aa917a4c9cb741ae5c8604ffee85fec2
docodepractices <tridang270395@gmail.com>
2024-06-17 21:30:31 +0700

    update

--- Test Suites/Test Suites/API.ts
+++ Test Suites/Test Suites/API.ts
@@ -10,82 +10,47 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>075609b2-a8b6-44a6-a93e-323432e81e49</testSuiteGuid>
+   <testSuiteGuid>0b21653b-deff-40d8-a86a-75a21e921e87</testSuiteGuid>
    <testCaseLink>
-      <guid>7ad825ea-085f-493b-ade9-d0342a365eea</guid>
+      <guid>9299790f-5616-431f-92b7-9c33abddb5ee</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/API01</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>37d10d4e-34aa-44bc-ba45-5860082d9559</guid>
+      <guid>b16c1d25-42d3-4dd6-8c1a-54d70fa41958</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/API01 - Copy - Copy - Copy - Copy - Copy - Copy - Copy - Copy - Copy - Copy - Copy</testCaseId>
+      <testCaseId>Test Cases/API010</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>e214c301-07c9-4988-b3e5-98ec310dcd2c</guid>
+      <guid>3ad0ced1-e873-406a-b23d-1261bac983f9</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/API01 - Copy - Copy - Copy - Copy - Copy - Copy - Copy - Copy - Copy - Copy</testCaseId>
+      <testCaseId>Test Cases/API011</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>150ae767-66c9-4128-8319-832f7a945840</guid>
+      <guid>d61fd0b5-8204-4739-9df1-5e66f9de6cc6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/API01 - Copy - Copy - Copy - Copy - Copy - Copy - Copy - Copy - Copy</testCaseId>
+      <testCaseId>Test Cases/API012</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>65f86d1e-8302-41e0-8d90-cb10b6ef4fba</guid>
+      <guid>36f1673a-b6f0-4cb3-8c7d-e40e841da99b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/API01 - Copy - Copy - Copy - Copy - Copy - Copy - Copy - Copy</testCaseId>
+      <testCaseId>Test Cases/API013</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>d176cc98-626d-48ea-8948-3da03bf67e38</guid>
+      <guid>2ed4eae3-ff3a-4ec7-92a0-617081ee24d6</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/API01 - Copy - Copy - Copy - Copy - Copy - Copy - Copy</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>525bfb1b-dc9e-4ba7-92c7-257625f37324</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API01 - Copy - Copy - Copy - Copy - Copy - Copy</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>b54e5fc6-9fc2-4de1-957c-d13b203a2ca3</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API01 - Copy - Copy - Copy - Copy - Copy</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>38cd880f-ac8a-4815-a9ff-2aa698b04a24</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API01 - Copy - Copy - Copy - Copy</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>5a9210fe-6ce6-4883-aa01-c81d1fa24a01</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API01 - Copy - Copy - Copy</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>f75b4d39-e374-422d-ab47-2c9d6db8d6af</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/API01 - Copy - Copy</testCaseId>
+      <testCaseId>Test Cases/API014</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
